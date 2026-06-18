275ca8383eef2aba3f0a3bbbc3d052c354ed128d
Júlio Cesar <julio.csantos.mail@gmail.com>
2026-06-18 14:23:58 -0300

    Adicionando o header da página

---
+++ src/scripts/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#hamburguer").on("click", function() {
+        $("#mobileNavigationLinks").toggleClass("active");
+        $("#mobileDefaultButton").toggleClass("active");
+        $("#hamburguer").find("i").toggleClass("fa-x");
+    });
+});
