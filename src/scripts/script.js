6dee87d398a043398857fe5364bf722c551bafc2
Júlio Cesar <julio.csantos.mail@gmail.com>
2026-06-29 01:07:49 -0300

    Adicionando animações no header e scrollReveal na página

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,4 +4,59 @@ $(document).ready(function () {
     $("#mobileDefaultButton").toggleClass("active");
     $("#hamburguer").find("i").toggleClass("fa-x");
   });
+
+  const sections = $("section");
+  const navItens = $(".link");
+
+  $(window).on("scroll", function () {
+    const header = $("header");
+    const scrollPosicao = $(window).scrollTop() - header.outerHeight();
+
+    let sectionIndex = 0;
+
+    if(scrollPosicao <= 0) {
+        header.css("box-shadow", "none");
+    } else {
+        header.css("box-shadow", "5px 1px 5px rgba(0, 0, 0, 0.51)");
+    }
+
+    sections.each(function(i) {
+        const section = $(this);
+
+        const topSection = section.offset().top - 96;
+        const botSection = topSection + section.outerHeight();
+
+        if(scrollPosicao > topSection && scrollPosicao < botSection) {
+            sectionIndex = i;
+            return false;
+        }
+    });
+
+    navItens.removeClass("bar");
+    $(navItens[sectionIndex]).addClass("bar");
+  });
+
+  ScrollReveal().reveal("#cta", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%"
+  }); 
+
+  ScrollReveal().reveal(".prato", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%"
+  }); 
+
+  ScrollReveal().reveal("#chefSection", {
+    origin: "left",
+    duration: 1000,
+    distance: "20%"
+  }); 
+
+  ScrollReveal().reveal(".depoimento", {
+    origin: "right",
+    duration: 1000,
+    distance: "20%"
+  }); 
 });
